4b480bd7f16c7c710b8ae4b668c172ad71d60da7
SirajMM <sirajmuhammed718@gmail.com>
2024-02-05 16:47:42 +0530

    first portion completed

--- lib/pages/containers/container1.dart
+++ lib/pages/containers/container1.dart
@@ -11,7 +11,61 @@ class Container1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenTypeLayout.builder(
       desktop: (context) => desktopContainer1(),
-      mobile: (context) => desktopContainer1(),
+      mobile: (context) => mobileContainer1(),
+    );
+  }
+
+  Widget mobileContainer1() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: w! / 10, vertical: 20),
+      child: Column(
+        children: [
+          Container(
+            height: w! / 1.2,
+            width: w! / 1.2,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(illustration1), fit: BoxFit.contain)),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Track your \nExpenses to \nSave Money',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: w! / 10, fontWeight: FontWeight.bold, height: 1),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            'Helps you to organize your income and expenses',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.grey.shade600, fontSize: 16),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            height: 45,
+            child: ElevatedButton.icon(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(AppColors.primary)),
+                onPressed: () {},
+                icon: const Icon(Icons.arrow_drop_down),
+                label: const Text('Try a Demo')),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            '— Web, iOs and Android',
+            style: TextStyle(color: Colors.grey.shade600, fontSize: 16),
+          )
+        ],
+      ),
     );
   }
 
@@ -33,7 +87,7 @@ class Container1 extends StatelessWidget {
                   const SizedBox(height: 20),
                   Text(
                     'Helps you to organize your income and expenses',
-                    style: TextStyle(color: Colors.grey.shade400, fontSize: 16),
+                    style: TextStyle(color: Colors.grey.shade400, fontSize: 20),
                   ),
                   const SizedBox(height: 20),
                   Row(
@@ -53,7 +107,7 @@ class Container1 extends StatelessWidget {
                       Text(
                         '— Web, iOs and Android',
                         style: TextStyle(
-                            color: Colors.grey.shade400, fontSize: 16),
+                            color: Colors.grey.shade400, fontSize: 20),
                       )
                     ],
                   )
@@ -62,7 +116,7 @@ class Container1 extends StatelessWidget {
             ),
             Expanded(
               child: Container(
-                height: 530,
+                height: w! / 4,
                 decoration: const BoxDecoration(
                     image: DecorationImage(
                         image: AssetImage(illustration1), fit: BoxFit.contain)),
